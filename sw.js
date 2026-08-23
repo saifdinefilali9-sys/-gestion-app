e2d1584aa37e438530881263c03ded40064dbc04
saifdinefilali9-sys <saifdinefilali9@gmail.com>
2026-08-23 01:08:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = "gestion-app-v1";
+const CACHE_NAME = "gestion-app-v2";
 
-const FILES = [
+const APP_FILES = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./sw.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES);
+      return cache.addAll(APP_FILES);
     })
   );
 
@@ -31,9 +32,34 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
     caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request)
+        .then(response => {
+
+          if (
+            response &&
+            response.ok &&
+            new URL(event.request.url).origin === self.location.origin
+          ) {
+            const copy = response.clone();
+
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, copy);
+            });
+          }
+
+          return response;
+        })
+        .catch(() => {
+          return caches.match("./index.html");
+        });
     })
   );
 });
